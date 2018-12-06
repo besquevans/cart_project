fbc12a8f1c9aca0c5009e019a7055391561f43a1
Jimmy <du20783@gmail.com>
2018-12-06 16:19:27 +0800

    setup relation between product, cart and cartltem

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
   validates_presence_of :name #必填
   mount_uploader :image, PhotoUploader #圖片上傳器
+
+  has_many :cartltems
+  has_many :carts, through: :cartltems
 end
