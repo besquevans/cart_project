87c1fff652c69b801379f573666800d904265fee
Jimmy <du20783@gmail.com>
2018-12-06 17:36:45 +0800

    Revert "create cartltem model"
    This reverts commit fa12bc43e85d37b41cb89b870aa9d7deaed915e5.

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_06_074959) do
-
-  create_table "cartltems", force: :cascade do |t|
-    t.integer "cart_id"
-    t.integer "product_id"
-    t.integer "quantity"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 2018_12_06_060516) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
