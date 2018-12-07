488056b1a4d3a8211f0f02e70e46bdf304c8fdf7
Jimmy <du20783@gmail.com>
2018-12-07 10:41:17 +0800

    add routes cart and add_to_cart for product

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
     root "products#index"
   end
 
-  resources :products, only: [:index, :show]
-  resource :cart
+  resources :products, only: [:index, :show] do 
+    post :add_to_cart, on: :member
+  end
+  
+  resource :cart   #當你設定單數資源時，網址的表達會變成 /cart 而不是 /cart/:id，就會直接對應到 show action
 end
