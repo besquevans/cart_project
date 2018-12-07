5c245853bab47c939f858d1716970c2d7cc6656a
Jimmy <du20783@gmail.com>
2018-12-07 11:39:41 +0800

    add current_cart and set_cart action for application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,22 @@
 class ApplicationController < ActionController::Base
-before_action :authenticate_user!
+  before_action :authenticate_user!
 
+  helper_method :current_cart
+
+  private
+
+  def current_cart
+    @cart || set_cart #return @cart if @cart exist, or call set_cart
+  end
+
+  def set_cart
+    if session[:cart_id]
+      @cart = Cart.find_by(id: session[:cart_id])
+    end
+
+    @cart ||= Cart.create
+
+    session[:cart_id] = @cart.id
+    @cart
+  end
 end
