3d5415cea4feab78e04c911f12ffee7bce11681c
Jimmy <du20783@gmail.com>
2018-12-10 17:45:50 +0800

    add routes remove_from_cat and adjust_item for products

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
 
   resources :products, only: [:index, :show] do 
     post :add_to_cart, on: :member
+    post :remove_from_cart, on: :member
+    post :adjust_item, on: :member
   end
   
   resource :cart   #當你設定單數資源時，網址的表達會變成 /cart 而不是 /cart/:id，就會直接對應到 show action
