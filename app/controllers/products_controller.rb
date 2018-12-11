b6d13bdaf153e875dfbd5a9b3b9635a1834b9a67
Jimmy <du20783@gmail.com>
2018-12-11 10:36:44 +0800

    add remove_from_cart action for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,17 +1,35 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :add_to_cart, :remove_from_cart, :adjust_item]
   
   def index
     @products = Product.page(params[:page]).per(10)
   end
 
   def show
-    @product = Product.find(params[:id])
+
   end
 
-  def add_to_cart
-    @product = Product.find(params[:id])
+  def add_to_cart   
     current_cart.add_cart_item(@product)
 
-    redirect_to root_path
+    redirect_back(fallback_location: root_path)
+  end
+
+  def remove_from_cart
+    cart_item = current_cart.cart_items.find_by(product_id: @product)
+    cart_item.destroy
+    redirect_back(fallback_location: root_path)
+  end
+
+  def adjust_item
+    cart_item = current_cart.cart_items.find_by(product_id: @product)
+
+    
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
