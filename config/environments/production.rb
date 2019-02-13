062a8fcdb24071cf3c56df5e12a2561677b9b3e7
Jimmy <du20783@gmail.com>
2019-02-13 14:32:00 +0800

    production email setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,13 +63,13 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "cart_project_#{Rails.env}"
 
-  #config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: "https://first-cart-project.herokuapp.com" }
-  config.action_mailer.smtp_settings = config_for(:application).symbolize_keys
+  #config.action_mailer.default_url_options = { host: "https://first-cart-project.herokuapp.com" }
+  #config.action_mailer.smtp_settings = config_for(:application).symbolize_keys
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
