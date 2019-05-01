66b73d6dfc7c4dc2ccdde8564a03a0a394492d4d
Jimmy <du20783@gmail.com>
2019-05-01 15:19:13 +0800

    add helper show_payment_methods

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,7 @@
 module OrdersHelper
+  def show_cancer_order(order)
+    if order.shipping_status == "not_shipped"
+      content_tag(:span, link_to("Cancell Order", order_path(order), method: :patch, class: "btn btn-default"))
+    end
+  end
 end
