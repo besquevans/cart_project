21a5533cf779138b69c17b01f42bf9bfb11cad87
Jimmy <du20783@gmail.com>
2019-05-08 00:02:40 +0800

    add order index page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
 
   def index
-    @orders = current_user.orders.order(created_at: :desc)
+    @orders = current_user.orders.order(created_at: :desc).page(params[:page]).per(5)
   end
 
   def create
