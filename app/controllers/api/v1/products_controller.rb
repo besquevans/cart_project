218284ad7bbd71b79a17673aaa190f41e3e15114
Jimmy <du20783@gmail.com>
2019-05-31 23:28:45 +0800

    add index and show action for product api

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,2 +1,32 @@
 class Api::V1::ProductsController < ApiController
+  def index 
+    @products = Product.find_each
+    render json: {
+      data: @products.map do |product|
+        {
+          name: product.name,
+          description: product.description,
+          price: product.price,
+          image: product.image
+        }
+      end
+    }
+  end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+    if !@product
+      render json: {
+        message: "Can't find the product!",
+        status: 400
+      }
+    else
+      render json: {
+        name: @product.name,
+        description: @product.description,
+        price: @product.price,
+        image: @product.image
+      }
+    end
+  end
 end
