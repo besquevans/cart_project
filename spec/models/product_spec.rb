72ea36f16b553edbb37af903848e876017684a5a
Jimmy <du20783@gmail.com>
2019-07-02 22:16:33 +0800

    add product model test

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,11 @@
+RSpec.describe Product, type: :model do
+  describe 'Association' do
+    it { should have_many(:cart_items) }
+    it { should have_many(:carts) }
+  end
+
+  describe 'Validation' do
+    subject { FactoryBot.create(:product) }
+    it { should validate_presence_of(:name) }   #product必須有name
+  end
+end
