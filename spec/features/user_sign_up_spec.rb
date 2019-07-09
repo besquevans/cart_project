5a68bd7af78dd5da1c800b418eb6434d542d1ed4
Jimmy <du20783@gmail.com>
2019-07-09 23:15:50 +0800

    add feature test sign up

---
+++ spec/features/user_sign_up_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+feature "user sign up" do
+
+  scenario "signing in with correct credentials" do
+    visit new_user_registration_path
+    
+    fill_in 'Email', :with => 'usertest@example.com'
+    fill_in 'Password', :with => 'caplin'
+    fill_in 'Password confirmation', :with => 'caplin'
+    click_button 'Sign up'
+      
+    expect(User.count).to eq(1) # you can test model
+    expect(page).to have_content 'Welcome! You have signed up successfully.' 
+  end
+  
+end
