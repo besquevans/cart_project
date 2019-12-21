b5947ef20db2f3b858383ae36216f609fde19d87
Jimmy <du20783@gmail.com>
2019-12-21 17:44:00 +0800

    add google login

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: %i[facebook]
+         :omniauthable, omniauth_providers: %i[facebook, google_oauth2]
 
   has_many :orders, dependent: :destroy
 
@@ -23,14 +23,25 @@ class User < ApplicationRecord
 
 #facebook傳入的登入資料
   def self.from_omniauth(auth)
+    data = auth.info
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
+      user.email = data.email
       user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name   # assuming the user model has a name
-      user.avatar = auth.info.image # assuming the user model has an image
+      user.name = data.name   # assuming the user model has a name
+      user.avatar = data.image # assuming the user model has an image
       # If you are using confirmable and the provider(s) you use validate emails, 
       # uncomment the line below to skip the confirmation emails.
       # user.skip_confirmation!
+    end
+  end
+
+  #google傳入的登入資料
+  def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
+    data = access_token.info
+    where(:google_token => access_token.credentials.token, :google_uid => access_token.uid ).first_or_create do |user| 
+      user.name = data["name"]
+      user.email = data["email"]
+      user.password = Devise.friendly_token[0,20]
     end
   end
 
